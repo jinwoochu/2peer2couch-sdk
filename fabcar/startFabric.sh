01d2820275d099009e0ef53f59feca4430e88ca6
jinwoo <cjw0672@naver.com>
2017-11-21 17:55:44 +0900

    first update

---
+++ fabcar/startFabric.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error
+set -e
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+starttime=$(date +%s)
+
+# launch network; 
+cd ../basic-network
+./start.sh
+
+# Now launch the CLI container in order to install, instantiate chaincode
+# and prime the ledger with our 10 cars
+docker-compose -f ./docker-compose.yml up -d cli
+
+
+# Create the channel
+CHANNEL_NAME=mychannel
+
+
+echo "-----------------------------------------"
+echo "Create channel."
+echo "-----------------------------------------"
+docker exec cli peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/channel.tx
+
+# Join peer0.org1.example.com to the channel.
+echo "-----------------------------------------"
+echo "Join peer0.org1.example.com to the channel."
+echo "-----------------------------------------"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051" cli peer channel join -b $CHANNEL_NAME.block
+
+# Join peer1.org1.example.com to the channel.
+echo "-----------------------------------------"
+echo "Join peer1.org1.example.com to the channel."
+echo "-----------------------------------------"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" cli peer channel join -b $CHANNEL_NAME.block
+
+
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n fabcar -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+sleep 10
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"function":"initLedger","Args":[""]}'
+
+printf "\nTotal setup execution time : $(($(date +%s) - starttime)) secs ...\n\n\n"
+printf "Start by installing required packages run 'npm install'\n"
+printf "Then run 'node enrollAdmin.js', then 'node registerUser'\n\n"
+printf "The 'node invoke.js' will fail until it has been updated with valid arguments\n"
+printf "The 'node query.js' may be run at anytime once the user has been registered\n\n"
